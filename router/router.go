4eec6fd496890ba781ad7b2014fc38e063e24c42
Tsigemariam Zewdu <tsigemariamzewdu20@gmail.com>
2025-07-16 10:33:06 +0300

    call controllers in router.go

--- router/router.go
+++ router/router.go
@@ -1,14 +1,15 @@
 package router
 
 import (
-	
+	"task_management/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func SetUpRoutes(router *gin.Engine){
-	router.GET("/tasks")
-	router.GET("/tasks/:id")
-	router.PUT("/tasks/:id")
-	router.DELETE("/tasks/:id")
-	router.POST("/tasks")
+	router.GET("/tasks",controllers.GetTasks)
+	router.GET("/tasks/:id",controllers.GetTaskByID)
+	router.PUT("/tasks/:id",controllers.UpdateTaskByID)
+	router.DELETE("/tasks/:id",controllers.GetTaskByID)
+	router.POST("/tasks",controllers.AddTask)
 }
